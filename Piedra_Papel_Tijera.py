04a3aa33f87ee0a95badeac3aea72400a36c2acf
TheCode <thecode@TheCodes-iMac.local>
2023-08-05 15:48:32 -0600

    Se arreglan Bugs de piedra papel o tijera

--- Piedra_Papel_Tijera.py
+++ Piedra_Papel_Tijera.py
@@ -6,7 +6,6 @@ papel
 tijeras
 """
 def piedra_papel_tijera(objeto_1, objeto_2 ,jugardor_1,jugardor_2):
-    
     if  objeto_1 == objeto_2 :
          print("")
          print("Hay empate intente de  nuevo :(  ")
@@ -16,16 +15,18 @@ def piedra_papel_tijera(objeto_1, objeto_2 ,jugardor_1,jugardor_2):
     elif objeto_1.lower() == "piedra" or objeto_2.lower() == "papel":
          Ganador = jugardor_2
          print(f"El Ganador es :{jugardor_2}")
+    elif objeto_1.lower() == "piedra" and objeto_2.lower() == "tijera":
+         Ganador = jugardor_1
+         print(f"El Ganador es :{jugardor_1}")
     elif objeto_1.lower() == "tijera" and objeto_2.lower() == "piedra":
          Ganador = jugardor_2
-    elif objeto_1.lower() == "piedra" and objeto_2.lower() == "tijera":
-         Ganador = jugardor_2
+         print(f"El Ganador es :{jugardor_2}")
     elif objeto_1.lower() == "tijera" and objeto_2.lower() == "papel":
          Ganador = jugardor_1
          print(f"El Ganador es :{jugardor_1}")
     elif objeto_1.lower() == "papel" and objeto_2.lower() == "tijera":
          Ganador = jugardor_2
-         print(f"El Ganador del juego es :{jugardor_2}")
+         print(f"El Ganador es :{jugardor_2}")
     while True: 
          opcion = input("Desea jugar de nuevo? (Y|N) : ")
          if opcion.lower() == "y" :
@@ -35,6 +36,7 @@ def piedra_papel_tijera(objeto_1, objeto_2 ,jugardor_1,jugardor_2):
             except: 
                 print("Ingreso un valor incorrecto intente de nuevo ")
             resultado =  Piedra_Papel_Tijera.piedra_papel_tijera(valor_1,valor_2,jugardor_1,jugardor_2)
+            print(f"El resultado del juego es {resultado}")
          elif opcion.lower() == "n":
             Menu_Principal.menu_principal()
          else :
